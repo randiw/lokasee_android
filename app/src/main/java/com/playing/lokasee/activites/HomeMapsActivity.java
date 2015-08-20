f8ff7e1e15449ebf2e4ae8f5a5001e2b06265d57
Randi Waranugraha <randi.waranugraha@gmail.com>
2015-08-20 10:25:54 +0700

    - change the Map API Key to without restriction - change the map to MainActivity

--- app/src/main/java/com/playing/lokasee/activites/HomeMapsActivity.java
+++ app/src/main/java/com/playing/lokasee/activites/HomeMapsActivity.java
@@ -1,19 +1,15 @@
 package com.playing.lokasee.activites;
 
-import android.content.Context;
 import android.location.Location;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
-import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.parse.CountCallback;
 import com.parse.FindCallback;
 import com.parse.GetCallback;
@@ -22,66 +18,36 @@ import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.SaveCallback;
 import com.playing.lokasee.R;
-import com.playing.lokasee.model.User;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import pl.charmas.android.reactivelocation.ReactiveLocationProvider;
-import rx.Observable;
 import rx.Subscription;
 import rx.functions.Action1;
-import rx.functions.Func1;
 
 /**
  * Created by mexan on 8/18/15.
  */
-public class HomeMapsActivity extends BaseActivity implements HomeMapsView, OnMapReadyCallback {
+public class HomeMapsActivity extends BaseActivity implements OnMapReadyCallback {
 
+    private static final String TAG = HomeMapsActivity.class.getSimpleName();
 
-    private Context mContext;
+    private GoogleMap googleMap;
+
     private Subscription subCript;
     private Double lat, lng;
     private ArrayList<Marker> mMarkers;
-    private GoogleMap gMap;
     private ArrayList<String> userData;
     int num;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mContext = this;
-        initView();
-        initData();
-    }
+        setupLayout(R.layout.home_maps);
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-    }
-
-    @Override
-    public void initView() {
-        setContentView(R.layout.home_maps);
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-    }
-
-    @Override
-    public void mapsSetup() {
-
-    }
-
-    private void initData(){
-        userData = GetSharedPrefs(mContext);
     }
 
     private void drawMarker(final GoogleMap nMap) {
@@ -126,10 +92,9 @@ public class HomeMapsActivity extends BaseActivity implements HomeMapsView, OnMa
     }
 
 
-
-    private void isDataExists(final String userId, final Double lat, final Double lng){
+    private void isDataExists(final String userId, final Double lat, final Double lng) {
         ParseQuery<ParseObject> query = ParseQuery.getQuery("Location");
-        query.whereEqualTo("fbId", dataUser.get(0));
+//        query.whereEqualTo("fbId", dataUser.get(0));
         query.countInBackground(new CountCallback() {
             @Override
             public void done(int i, ParseException e) {
@@ -146,17 +111,17 @@ public class HomeMapsActivity extends BaseActivity implements HomeMapsView, OnMa
         });
     }
 
-    private void saveData(boolean isExist, String userId,  final Double lat, final Double lng){
+    private void saveData(boolean isExist, String userId, final Double lat, final Double lng) {
 
-        if(!isExist){
+        if (!isExist) {
 
             ParseObject dataObj = new ParseObject("Location");
-            dataObj.put("fbId", dataUser.get(0));
+//            dataObj.put("fbId", dataUser.get(0));
             dataObj.put("latitude", lat);
             dataObj.put("longitude", lng);
             dataObj.saveInBackground();
 
-        }else{
+        } else {
 
             ParseQuery<ParseObject> query = ParseQuery.getQuery("Location");
             query.whereEqualTo("fbId", userId);
@@ -173,47 +138,35 @@ public class HomeMapsActivity extends BaseActivity implements HomeMapsView, OnMa
 
 
             // If done let's draw marker
-            drawMarker(gMap);
+//            drawMarker(gMap);
         }
 
     }
 
 
-
-
-
     @Override
     public void onMapReady(final GoogleMap googleMap) {
-        ReactiveLocationProvider locationProvider = new ReactiveLocationProvider(mContext);
+        this.googleMap = googleMap;
 
-        locationProvider.getLastKnownLocation()
-                .subscribe(new Action1<Location>() {
-                    @Override
-                    public void call(Location location) {
+        ReactiveLocationProvider locationProvider = new ReactiveLocationProvider(getApplicationContext());
+        locationProvider.getLastKnownLocation().subscribe(new Action1<Location>() {
+            @Override
+            public void call(Location location) {
+                lat = location.getLatitude();
+                lng = location.getLongitude();
 
-
-                        lat = location.getLatitude();
-                        lng = location.getLongitude();
-
-                        // if location detected show marker
-                        setupMaps(googleMap, lat, lng);
-
-                    }
-                });
+                // if location detected show marker
+                setupMaps(lat, lng);
+            }
+        });
     }
 
 
-
-
-    private void setupMaps(GoogleMap googleMap, Double lat, Double lon){
-
-
+    private void setupMaps(Double lat, Double lon) {
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lat, lon), 14));
 
-        gMap = googleMap;
-
         // Update current lat & lon to parse
-        isDataExists(dataUser.get(0), lat, lon);
+//        isDataExists(dataUser.get(0), lat, lon);
     }
 
 
@@ -240,19 +193,19 @@ public class HomeMapsActivity extends BaseActivity implements HomeMapsView, OnMa
 
                             if (e == null) {
 
-                                Log.i(getLocalClassName(),"Successs");
+                                Log.i(getLocalClassName(), "Successs");
 
                                 drawMarker(googleMap);
 
 
-                            }else{
+                            } else {
 
                                 e.printStackTrace();
                             }
                         }
                     });
 
-                }else{
+                } else {
 
                     e.printStackTrace();
 
@@ -260,5 +213,4 @@ public class HomeMapsActivity extends BaseActivity implements HomeMapsView, OnMa
             }
         });
     }
-
 }
